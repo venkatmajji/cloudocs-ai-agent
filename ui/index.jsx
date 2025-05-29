cc77d543c40a7c526fe5546bae26f6ac1abe628c
venkatmajji <52803952+venkatmajji@users.noreply.github.com>
2025-05-29 16:57:59 -0400

    Create index.jsx

---
+++ ui/index.jsx
@@ -0,0 +1,11 @@
+// ui/index.jsx
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LandingPage from "./app.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LandingPage />
+  </React.StrictMode>
+);
