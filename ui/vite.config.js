5932990b69141ea18f2030fa05ae09da1e436f1d
venkatmajji <52803952+venkatmajji@users.noreply.github.com>
2025-05-29 18:48:14 -0400

    Update vite.config.js

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,11 +1,8 @@
-// ui/vite.config.js
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
@@ -14,3 +11,4 @@ export default defineConfig({
     port: 3000,
   },
 });
+
